83013fbc0d27b9f1caa795bb8fd806dfdb8a20a6
WhatQiatetoday <347746083@qq.com>
2025-08-08 18:14:05 +0800

    一些修改

--- apps/client/assets/Scripts/Entity/Weapon/WeaponManager.ts
+++ apps/client/assets/Scripts/Entity/Weapon/WeaponManager.ts
@@ -1,8 +1,9 @@
-import { _decorator, instantiate } from 'cc';
+import { _decorator, instantiate, Node } from 'cc';
 import { EntityManager } from '../../Base/EntityManager';
 import { EntityTypeEnum, IActor } from '../../Common';
-import { EntityStateEnum } from '../../Enum';
+import { EntityStateEnum, EventEnum } from '../../Enum';
 import DataManager from '../../Global/DataManager';
+import EventManager from '../../Global/EventManager';
 import { WeaponStateMachine } from './WeaponStateMachine';
 const { ccclass, property } = _decorator;
 
@@ -13,11 +14,22 @@ export class WeaponManager extends EntityManager {
     private point: Node = null;
 
     init(data: IActor) {
+        this.body = this.node.getChildByName("Body");
+        this.anchor = this.body.getChildByName("Anchor");
+        this.point = this.anchor.getChildByName("Point");
+
         this.fsm = this.node.addComponent(WeaponStateMachine);
         this.fsm.init(data.weaponType);
         this.state = EntityStateEnum.Idle;
         const weapon = instantiate(DataManager.Instance.prefabMap.get(EntityTypeEnum.Weapon1));
         weapon.parent = this.node;
+
+        EventManager.Instance.on(EventEnum.WeaponShoot, this.handleWeaponShoot, this);
     }
+
+    handleWeaponShoot() {
+
+    }
+
 }
 
---
+++ apps/client/assets/Scripts/UI/ShootManager.ts
@@ -0,0 +1,21 @@
+import { _decorator, Component } from 'cc';
+import { EventEnum } from '../Enum';
+import EventManager from '../Global/EventManager';
+const { ccclass, property } = _decorator;
+
+@ccclass('ShootManager')
+export class ShootManager extends Component {
+
+    handleShoot() {
+        EventManager.Instance.emit(EventEnum.WeaponShoot);
+    }
+
+    protected onLoad(): void {
+
+    }
+
+    update(deltaTime: number) {
+
+    }
+}
+
